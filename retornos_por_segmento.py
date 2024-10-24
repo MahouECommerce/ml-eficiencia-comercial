b94d16ee46e0630ec92a82d2093f78da058def43
ctrujils <ctrujils@emeal.nttdata.com>
2024-10-24 09:48:43 +0200

    update: cambiado el df de origen de los segmentos

--- retornos_por_segmento.py
+++ retornos_por_segmento.py
@@ -53,6 +53,7 @@ path = r'C:\Users\ctrujils\eficiencia_promocional\0. Datos\\'
 
 file_modelo= 'df_modelo.parquet'
 pickle_modelo='modelo_general.pkl'
+segmentos_df= 'order_detail_grouped_nuevos_segmentos.parquet'
 
 df = pd.read_parquet(f'{path}{file_modelo}')
 df.CouponDiscountAmt.sum()
@@ -61,60 +62,34 @@ modelo_path=f'{path}{pickle_modelo}'
 with open(modelo_path, "rb") as f:
     modelo_general = pickle.load(f)
 
-### Carga de datos de 
+order_detail_grouped_segmentos=pd.read_parquet(f'{path}{segmentos_df}')
 
 
-
-#### DataFrame por pdv y % de cuponero
-
-df_pdv_coupon_usage = df.groupby('PointOfSaleId')['PctgCouponUsed'].mean().reset_index()
-
-
-#Definimos umbral 
-corte_cupon=[100, 99, 98, 95, 93, 90, 88, 85, 82, 80]
-
-#Creamos diccionario para almacenar dfs
-df_por_corte={}
-enumerate_corte=enumerate(corte_cupon, start=1)
-for idx , coupon in enumerate_corte:
-   df_corte= df_pdv_coupon_usage[df_pdv_coupon_usage['PctgCouponUsed']> coupon]
-
-   df_por_corte[f'df_cuponeros_corte_{coupon}']=df_corte
-
-
-df_filtrados={}
-## Sacamos los dfs con los pdvs de cada df del dccionario de df_por_corte
-for i, df_pdv in df_por_corte.items():
-    df_corte=df[df['PointOfSaleId'].isin(df_pdv['PointOfSaleId'])]
-
-    df_filtrados[f'{i}']=df_corte
-
-#  guardar en csv
+#
+# #guardar en csv
 # for key, filtered_df in df_filtrados.items():
 #     filtered_df.to_csv(f'{key}.csv', index=False)
 
-# grupos = [
-#     {"nombre": "dormidos", "df": dormidos},
-#     # {"nombre": "offline", "df": offline},
-#     {"nombre": "cuponeros", "df": df_cuponeros.PointOfSaleId},
-#     {"nombre": "optimos", "df": optimos},]
+grupos = grupos = [
+    {"nombre": "dormidos", "df": order_detail_grouped_segmentos[order_detail_grouped_segmentos['segmento'] == 'dormidos']},
+    {"nombre": "offline", "df": order_detail_grouped_segmentos[order_detail_grouped_segmentos['segmento'] == 'offline']},
+    {"nombre": "cuponeros", "df": order_detail_grouped_segmentos[order_detail_grouped_segmentos['segmento'] == 'cuponeros']},
+    {"nombre": "optimos", "df": order_detail_grouped_segmentos[order_detail_grouped_segmentos['segmento'] == 'optimos']}
+]
 
 
 
 
-grupos = [{"nombre": key, "df": df_filtrados[key]['PointOfSaleId']} for key in df_filtrados]
-
-
-# for g in grupos:
-#     print(g["nombre"])
-#     gdf = df[df.PointOfSaleId.isin(g["df"].tolist())]
-#     br = BayesianRidge(fit_intercept=False)
-#     br.fit(gdf[feat_cols], gdf.Sellout)
-#     print(gdf.shape)
+for g in grupos:
+    print(g["nombre"])
+    gdf = df[df.PointOfSaleId.isin(g["df"].tolist())]
+    br = BayesianRidge(fit_intercept=False)
+    br.fit(gdf[feat_cols], gdf.Sellout)
+    print(gdf.shape)
     
-#     # Calcular y mostrar los retornos
-#     returns = compute_returns(gdf, br)
-#     print(f"Cuantiles de los retornos para el grupo {g['nombre']}: {np.quantile(np.array(returns), [0.05, 0.5, 0.95])}")
+    # Calcular y mostrar los retornos
+    returns = compute_returns(gdf, br)
+    print(f"Cuantiles de los retornos para el grupo {g['nombre']}: {np.quantile(np.array(returns), [0.05, 0.5, 0.95])}")
 
 
 
